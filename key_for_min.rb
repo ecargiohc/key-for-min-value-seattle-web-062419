3b5622c24350a530cc7970fb4ee127d04946eed9
Grace Colette <ilovegrace09@gmail.com>
2019-06-22 20:21:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
- name_hash.collect do |key,value|
+ name_hash.each do |key,value|
   if value < lowest_value
     lowest_key = key
     lowest_value = value
